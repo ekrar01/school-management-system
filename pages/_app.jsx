9134e3600d1e1e0a7ac7d57ed5081e6f73e0131b
ekrar <ekrarrazakhan@gmail.com>
2025-09-05 13:12:45 +0530

    update

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,4 @@
-import Header from '../components/Layout/Header';
+import Header from '../components/layout/Header';
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
